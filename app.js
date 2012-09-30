06aab624b01403ea19ff52240a3f90f7d94fde00
Michael Bosworth <mtbosworth@gmail.com>
2012-09-29 20:15:26 -0500

    almost done with login

--- app.js
+++ app.js
@@ -4,21 +4,11 @@
  */
 
 var express = require('express'),
-    _ = require('underscore'),
     mongo = require('mongodb'),
     hbs = require('hbs'),
-    store  = new express.session.MemoryStore;
+    app = module.exports = express();
 
-var db = new mongo.Db('donis', new mongo.Server("127.0.0.1", 27017, {}));
-db.open(function(err, p_db) {
-  db.emit('connect', db);
-});
-
-
-var app = module.exports = express.createServer();
-app.set('db', db);
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('db', db);
@@ -27,11 +17,35 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'theBestWifeAGuyCouldAskFor', store: store }));
+  app.use(express.cookieParser('Alw@ysHaveAlw@ysWill'));
+  app.use(express.session());
 });
 
-hbs.registerPartial('index', 'index');
+// Connect to mongo
+var db = new mongo.Db('donis', new mongo.Server("127.0.0.1", 27017, {}));
+db.open(function(err, p_db) {
+  db.emit('connect', db);
+
+  // Setup admins
+  db.collection('users', function(err, collection) {
+
+    if(err){
+      console.log('error', err);
+    } else {
+      db.createCollection('users', function(err, collection){
+        createAdmins(collection);
+      });
+    }
+    
+  });   
+});
+
+// Define admins
+function createAdmins(collection){
+  // Update the document with an atomic operator
+  collection.update({username:'mtbosworth@gmail.com'}, {$set : {username:'mtbosworth@gmail.com', password:'6e4d5d18de1a83c179a3e6981bdf01ac278fb7b8'}}, {upsert:true});
+  collection.update({username:'donis.bosworth@gmail.com'}, {$set : {username:'donis.bosworth@gmail.com', password:'6ef819500b8661caf9c326d341d0079a1dcba499'}}, {upsert:true});
+}
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -46,5 +60,5 @@ var routes = require('./routes')(app, db);
 var routes = require('./apps/content-admin/routes')(app, db);
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
--- apps/content-admin/routes/index.js
+++ apps/content-admin/routes/index.js
@@ -4,10 +4,55 @@
  */
 function routes(app, db) {
 
-	app.get('/admin', function(req, res){
-      console.log(req.session);
+  var crypto = require('crypto');
 
-      if(req.session) {
+  // Authenticate using our plain-object database of doom!
+  function authenticate(username, password, fn) {
+    console.log('authenticate');
+    // if (!module.parent) console.log('authenticating %s:%s', username, password);
+
+    // Set users
+    db.collection('users', function(err, collection) {
+
+      if(err){
+        console.log('error', err);
+      }
+
+      // Update the document with an atomic operator
+      collection.findOne({username:username}, function(err, doc){
+        if(err || !doc){
+          new Error('Cannot Find User');
+        } else {
+          console.log('found user name');
+          var hash = crypto.createHmac('sha1', username).update(password).digest('hex');
+          console.log(hash);
+          collection.findOne({username:username, password:hash}, function(err, doc){
+            if(err || !doc){
+              new Error('invalid password')
+            } else {
+              console.log('and password');
+              console.log(doc);
+              return fn(null, doc);
+            }
+          });            
+
+        }
+
+      });
+      
+    }); 
+
+  }
+
+  app.get('/admin', function(req, res){
+      console.log(req.session);
+      req.session = req.session || {user:null};
+
+      if (req.session.user) {
+
+        req.session.success = 'Authenticated as ' + req.session.user.username
+      + ' click to <a href="/logout">logout</a>. '
+      + ' You may now access <a href="/restricted">/restricted</a>.';
         
         // Return the information of a all collections, using the callback format
         db.collections(function(err, collections) {
@@ -17,24 +62,48 @@ function routes(app, db) {
         });
 
       } else {
-        res.render(__dirname + '/../views/login.hbs', { title: 'Login' });
+        res.render(__dirname + '/../views/login.hbs', { title: 'Login', success: req.session.success, error:req.session.error  });
       }
 
-  });		
+  });   
 
-	app.get('/collection/:name', function(req, res){
-		
+  app.post('/admin', function(req, res){
+    console.log('post');
+    authenticate(req.body.username, req.body.password, function(err, user){
+      console.log('user',user);
+      if (user) {
+    
+          // Store the user's primary key 
+          // in the session store to be retrieved,
+          // or in this case the entire user object
+          console.log('req', req);
+          console.log('sesssion',req.session);
+          req.session.user = user;
+          res.redirect('admin');
+    
+      } else {
+        req.session.error = 'Authentication failed, please check your '
+          + ' username and password.'
+          + ' (use "tj" and "foobar")';
+        res.redirect('admin');
+      }
+    });
+
+  });     
+
+  app.get('/collection/:name', function(req, res){
+    
       // Return the information of a all collections, using the callback format
       db.collection(req.params.name, function(err, collection) {
 
-      	collection.find().toArray(function(err, items) {
+        collection.find().toArray(function(err, items) {
           res.render(__dirname + '/../views/collection.hbs', { title: req.params.name, collection:req.params.name, items:items });
         });
         
         
       });
 
-  });		  
+  });     
 
   app.get('/collection/:name/new', function(req, res){
       
@@ -52,6 +121,14 @@ function routes(app, db) {
     collection.insert(req.body);
 
   }); 
+
+  app.get('/logout', function(req, res){
+    // destroy the user's session to log them out
+    // will be re-created next request
+    req.session.destroy(function(){
+      res.redirect('/');
+    });
+  });  
 
 }
 
