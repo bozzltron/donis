edc50a66901d34a77d92d49812ee9bc6837ed7f8
Michael Bosworth <michael.bosworth@bunchball.com>
2014-11-23 14:52:29 -0600

    sendgrid integration

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,30 @@ function routes(app, db) {
 
 	app.post('/contact', function(req, res){
 	  // Send Email
+
+	  	var time = new Date().getTime();
+
+	  	if(time - parseInt(req.body.time, 10) > 5000) {
+
+		  	//process.env.SENDGRID_USERNAME
+		  	//process.env.SENDGRID_PASSWORD
+			var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+			sendgrid.send({
+			  to:       'mydouladonis@gmail.com',
+			  from:     req.body.email,
+			  subject:  'A Message From My Doula Donis',
+			  text:     'From : ' + req.body.name + ' ' + req.body.email + ' Message: '  + req.body.message
+			}, function(err, json) {
+			  if (err) { return console.error(err); }
+
+			  res.json(json);
+
+			});
+
+		} else {
+			res.json({status:"error", message:"You filled this out too fast to be a human."})
+		}
+
 	});	
 
 }
